32b93171b8c4fb2bb511b9357a1f8ee4e42e06f0
Andrew Pynch <andrewpynchbusiness@gmail.com>
2021-05-31 21:10:35 -0500

    get users

--- dappy/parser.py
+++ dappy/parser.py
@@ -1,7 +1,8 @@
 import collections
 import csv
+from warnings import showwarning
 from bs4 import BeautifulSoup
-from user import User
+from user import *
 
 
 class Dappy(object):
@@ -13,6 +14,18 @@ class Dappy(object):
     def __call__(self):
         pass
 
+    def get_all_users(self):
+        message_headers = self.soup.find_all("span", {"class": "chatlog__author-name"})
+        for header in message_headers:
+            user = get_user_from_message_header(header)
+            self.users[user.id].append(user.title)
+        self.users = get_user_list_from_dict(self.users)
+        self.show_all_users()
+
+    def show_all_users(self):
+        print(user for user in self.users)
+
 
 if __name__ == "__main__":
     dappy = Dappy("frostbite.html")
+    dappy.get_all_users()
--- dappy/user.py
+++ dappy/user.py
@@ -1,3 +1,6 @@
+from bs4 import BeautifulSoup
+
+
 class User(object):
     def __init__(self, id, title):
         self.id = id
@@ -8,3 +11,22 @@ class User(object):
 
     def __str__(self):
         return f"Id: {self.id} | Title: {self.title}"
+
+
+def get_user_from_message_header(message_header):
+    id = message_header["data-user-id"]
+    title = message_header["title"]
+    return User(id, title)
+
+
+def is_unique_user(user, users):
+    return any(search_user.id == user.id for search_user in users)
+
+
+def get_user_list_from_dict(user_dict):
+    users = []
+    for id in user_dict:
+        unique_titles = list(dict.fromkeys(user_dict[id]))
+        user = User(id, unique_titles)
+        users.append(user)
+    return users
